6b9e1eb91c6b40aac500d65e5d46df4557b82697
Shubhankar Das <76389456+SmartTech-SD@users.noreply.github.com>
2020-12-20 15:53:54 +0530

    Add files via upload

---
+++ AutoVideoPlay.py
@@ -0,0 +1,7 @@
+# importing pywhatkit
+from pywhatkit import playonyt
+
+# user input
+video_name=input('Enter video name you want to play on youtube: ')
+# using 'playonyt' attribute
+playonyt(video_name)
